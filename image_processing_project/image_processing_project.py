6bfba66560a6eadd6d69180d2394bc271b4fc754
AgnesResto <amresto@umich.edu>
2018-11-20 21:46:24 -0500

    Adding functions to main code

--- image_processing_project/image_processing_project.py
+++ image_processing_project/image_processing_project.py
@@ -21,12 +21,7 @@ def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
 
-    Replace this function and doc string for your own project
 
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
 
     Returns
     -------
